b9f5d949dbf8460626d994639578e7e8ce2d5c48
nekosora <thenekosora@gmail.com>
2019-05-25 06:03:24 +0600

    Normal calculation

---
+++ rt/Render/Calculator.cs
@@ -0,0 +1,10 @@
+﻿using OpenTK;
+
+namespace RT.Render
+{
+    static class Calculator
+    {
+        public static Vector3 CalculateNormal(Vector3 a, Vector3 b, Vector3 c) =>
+            Vector3.Normalize(Vector3.Cross(b - a, c - a));
+    }
+}
---
+++ rt/Render/Face.cs
@@ -0,0 +1,32 @@
+﻿using OpenTK;
+
+namespace RT.Render
+{
+    class Face
+    {
+        public const int VERTEX_AMOUNT = 4;
+
+        public readonly Vector3[] Positions;
+        public readonly Vector2[] TextureMap;
+        public readonly Vector3[] Normals;
+
+        public Face(Vector3[] positions, Vector2[] textureMap, Vector3[]? normals = null)
+        {
+            Positions = positions;
+            TextureMap = textureMap;
+
+            if (normals == null)
+            {
+                Vector3 normal = Calculator.CalculateNormal(
+                    positions[0],
+                    positions[1],
+                    positions[2]
+                    );
+
+                normals = new Vector3[] { normal, normal, normal, normal };
+            }
+
+            Normals = normals;
+        }
+    }
+}
